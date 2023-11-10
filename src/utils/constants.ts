a31dc3b39fa616955a426bc4fdd859d82a8c3fd9
zekunjin <zekun.jin@foxmail.com>
2023-11-10 17:51:28 +0800

    chore: use relative path

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,8 +4,8 @@ export const XRAY_CORE_RELEASE = 'https://github.com/XTLS/Xray-core/releases/dow
 export const XRAY_CORE_PLATFOFMS = { win32: 'windows', darwin: 'macos' }
 export const XRAY_CORE_ARCHES = { x64: '64', arm64: 'arm64' }
 export const XRAY_CORE_BIN = os.platform() === 'win32' ? 'xray.exe' : 'xray'
-export const XRAY_CORE_DIR = '.xray'
+export const XRAY_CORE_DIR = join(__dirname, '.xray')
 
-export const RAYNER_DIR = '.rayner'
+export const RAYNER_DIR = join(__dirname, '.rayner')
 
 export const STORAGE_OUTBOUNDS = 'outbounds'
