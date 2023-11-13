8ee725147bb742c56ba2f1e4dc265420a3685ef0
zekunjin <zekun.jin@foxmail.com>
2023-11-13 14:20:18 +0800

    chore: add xray profiles api

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -2,9 +2,11 @@ import postOutbounds from '~/routes/outbounds/index.post'
 import getOutbounds from '~/routes/outbounds/index.get'
 import deleteOutbounds from '~/routes/outbounds/index.delete'
 import patchOutbounds from '~/routes/outbounds/index.patch'
+import getXrayProfile from '~/routes/xray-profiles/[id]/index.get'
 
 export const router = createRouter()
   .post('/outbounds', postOutbounds)
   .get('/outbounds', getOutbounds)
   .delete('/outbounds', deleteOutbounds)
   .patch('/outbounds', patchOutbounds)
+  .get('/xray-profiles/:id', getXrayProfile)
