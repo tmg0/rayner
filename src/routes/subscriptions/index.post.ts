8513623cbab0307e3f50fa63eb107d6db8b635a1
zekunjin <zekun.jin@foxmail.com>
2023-11-14 13:00:16 +0800

    feat: support https unauth

--- src/routes/subscriptions/index.post.ts
+++ src/routes/subscriptions/index.post.ts
@@ -1,7 +1,9 @@
+import https from 'node:https'
+import { parseURL } from 'ufo'
 import { version } from '~~/package.json'
 
-export const VMESS_PREFIX = 'vmess://'
-export const SS_PREFIX = 'ss://'
+const VMESS_PREFIX = 'vmess://'
+const SS_PREFIX = 'ss://'
 
 interface Vmess {
   ps: string
@@ -48,19 +50,47 @@ export default eventHandler(async (event) => {
   const body = await readBody(event)
   await subscriptionStore.ads(body)
 
-  const response = await fetch(body.link, { headers: { 'User-Agent': `Rayner/${version}` } })
-  const decode = await response.text()
-  const encode = atob(decode)
+  const headers = { 'User-Agent': `Rayner/${version}` }
+  const agent = new https.Agent({ rejectUnauthorized: false })
 
-  const proxies = encode.split(/[\n\r]/).filter(Boolean).flat()
+  const { host, pathname } = parseURL(body.link)
 
-  const outbounds: RaynerOutbound[] = proxies.map((proxy) => {
-    if (isVmess(proxy)) { return parseVmess(proxy) }
-    if (isShadowsocks(proxy)) { return parseShadowsocks(proxy) }
-    return undefined
-  }).filter(Boolean)
+  const [hostname, port] = host.split(':')
 
-  await outboundStore.ado(outbounds)
+  const options = {
+    hostname,
+    port: Number(port),
+    method: 'GET',
+    path: pathname,
+    rejectUnauthorized: false // 禁用 SSL/TLS 证书验证
+  }
+
+  https.get(options, (res) => {
+    let data = ''
+
+    // 监听数据事件以接收数据块（chunks）
+    res.on('data', (chunk) => {
+      data += chunk
+    })
+
+    // 监听结束事件以完成数据接收
+    res.on('end', () => {
+      console.log(data) // 此时 data 是完整的响应体字符串
+    })
+  })
+
+  // const decode = await response.text()
+  // const encode = atob(decode)
+
+  // const proxies = encode.split(/[\n\r]/).filter(Boolean).flat()
+
+  // const outbounds: RaynerOutbound[] = proxies.map((proxy) => {
+  //   if (isVmess(proxy)) { return parseVmess(proxy) }
+  //   if (isShadowsocks(proxy)) { return parseShadowsocks(proxy) }
+  //   return undefined
+  // }).filter(Boolean)
+
+  // await outboundStore.ado(outbounds)
 
   return {}
 })
