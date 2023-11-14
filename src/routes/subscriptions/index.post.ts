fc98ab1f262af6c1953c24e6acc6e40c101a9186
zekunjin <zekun.jin@foxmail.com>
2023-11-14 13:10:46 +0800

    chore: use undici

--- src/routes/subscriptions/index.post.ts
+++ src/routes/subscriptions/index.post.ts
@@ -1,5 +1,5 @@
-import https from 'node:https'
-import { parseURL } from 'ufo'
+import https from 'https'
+import { request, Agent } from 'undici'
 import { version } from '~~/package.json'
 
 const VMESS_PREFIX = 'vmess://'
@@ -51,46 +51,26 @@ export default eventHandler(async (event) => {
   await subscriptionStore.ads(body)
 
   const headers = { 'User-Agent': `Rayner/${version}` }
-  const agent = new https.Agent({ rejectUnauthorized: false })
 
-  const { host, pathname } = parseURL(body.link)
-
-  const [hostname, port] = host.split(':')
-
-  const options = {
-    hostname,
-    port: Number(port),
-    method: 'GET',
-    path: pathname,
-    rejectUnauthorized: false // 禁用 SSL/TLS 证书验证
-  }
-
-  https.get(options, (res) => {
-    let data = ''
-
-    // 监听数据事件以接收数据块（chunks）
-    res.on('data', (chunk) => {
-      data += chunk
-    })
-
-    // 监听结束事件以完成数据接收
-    res.on('end', () => {
-      console.log(data) // 此时 data 是完整的响应体字符串
+  const { body: response } = await request(body.link, {
+    headers,
+    dispatcher: new Agent({
+      connect: { rejectUnauthorized: false }
     })
   })
 
-  // const decode = await response.text()
-  // const encode = atob(decode)
+  const decode = await response.text()
+  const encode = atob(decode)
 
-  // const proxies = encode.split(/[\n\r]/).filter(Boolean).flat()
+  const proxies = encode.split(/[\n\r]/).filter(Boolean).flat()
 
-  // const outbounds: RaynerOutbound[] = proxies.map((proxy) => {
-  //   if (isVmess(proxy)) { return parseVmess(proxy) }
-  //   if (isShadowsocks(proxy)) { return parseShadowsocks(proxy) }
-  //   return undefined
-  // }).filter(Boolean)
+  const outbounds: RaynerOutbound[] = proxies.map((proxy) => {
+    if (isVmess(proxy)) { return parseVmess(proxy) }
+    if (isShadowsocks(proxy)) { return parseShadowsocks(proxy) }
+    return undefined
+  }).filter(Boolean)
 
-  // await outboundStore.ado(outbounds)
+  await outboundStore.ado(outbounds)
 
   return {}
 })
